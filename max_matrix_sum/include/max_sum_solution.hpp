9563a2cb31ee0d14a2b136428451c9be53cd6b01
paler123 <jkowalisko@gmail.com>
2019-02-22 10:44:12 +0100

    added support for solving and retrieving path

--- max_matrix_sum/include/max_sum_solution.hpp
+++ max_matrix_sum/include/max_sum_solution.hpp
@@ -2,6 +2,7 @@
 
 #include "matrix_type_traits.hpp"
 #include <cstddef>
+#include <vector>
 
 namespace MaxSum
 {
@@ -9,7 +10,7 @@ namespace Details
 {
 
 template <typename T>
-struct IndexedRowValue
+struct PathEnd
 {
   T val;
   std::size_t index;
@@ -18,15 +19,62 @@ struct IndexedRowValue
 template <typename T>
 struct TopTwo
 {
-  IndexedRowValue<T> max;
-  IndexedRowValue<T> almost_max;
+  PathEnd<T> max;
+  PathEnd<T> almost_max;
+};
+
+struct DiscardPaths {
+  constexpr void init (std::size_t, std::size_t, std::size_t) {}
+  constexpr void adjust_ends(std::size_t, std::size_t)  {
+  // intentionally discard the values
+  }
+};
+
+struct StorePaths {
+  void init (std::size_t rows, std::size_t top, std::size_t almost_top)
+  {
+    top_path.reserve(rows);
+    almost_top_path.reserve(rows);
+    top_path.push_back(top);
+    almost_top_path.push_back(almost_top);
+  }
+  void adjust_ends(std::size_t top_end, std::size_t almost_top_end)  {
+    // one of paths definetely goes through top
+    if (path_covers_current_top_path(top_end)) {
+      if (path_covers_current_top_path(almost_top_end)) {
+        almost_top_path = top_path;
+        top_path.push_back(top_end);
+        almost_top_path.push_back(almost_top_end);
+      }
+      else {
+        top_path.push_back(top_end);
+        almost_top_path.push_back(almost_top_end);
+      }
+    }
+    else {
+      almost_top_path.push_back(top_end);
+      top_path.push_back(almost_top_end);
+      std::swap(top_path, almost_top_path);
+    }
+  }
+
+  std::vector<std::size_t> recover_top() {
+    return std::move(top_path);
+  }
+
+private:
+  std::vector<std::size_t> top_path;
+  std::vector<std::size_t> almost_top_path;
+  bool path_covers_current_top_path(std::size_t path_end) const {
+    return *top_path.crbegin() != path_end;
+  }
 };
 
 template <typename T>
 constexpr TopTwo<T> sorted_first_two(T v1, T v2)
 {
-  auto m1 = IndexedRowValue<T>{v1, 0};
-  auto m2 = IndexedRowValue<T>{v2, 1};
+  auto m1 = PathEnd<T>{v1, 0};
+  auto m2 = PathEnd<T>{v2, 1};
   return v1 >= v2 ? TopTwo<T>{m1, m2} : TopTwo<T>{m2, m1};
 }
 
@@ -36,10 +84,10 @@ constexpr void update_top(TopTwo<T>& top_paths, T path_value, std::size_t col)
   if (path_value > top_paths.max.val)
   {
     top_paths.almost_max = top_paths.max;
-    top_paths.max = IndexedRowValue<T>{path_value, col};
+    top_paths.max = PathEnd<T>{path_value, col};
   }
   else if (path_value > top_paths.almost_max.val)
-    top_paths.almost_max = IndexedRowValue<T>{path_value, col};
+    top_paths.almost_max = PathEnd<T>{path_value, col};
 }
 
 template <typename Matrix>
@@ -71,15 +119,35 @@ constexpr TopTwo<T> find_best_paths_for_row(TopTwo<T> const& top_last_row, std::
   return top_paths;
 }
 
-template <typename Matrix>
-constexpr auto solve_non_trivial(Matrix const& input)
+template <typename Matrix, typename PathsPolicy>
+constexpr auto solve_non_trivial(Matrix const& input, PathsPolicy && paths)
 {
   auto top_paths = find_top2_in_first_row(input);
-  for (auto i = 1u; i < input.rows(); ++i)
+  paths.init(input.rows(), top_paths.max.index, top_paths.almost_max.index);
+  for (auto i = 1u; i < input.rows(); ++i) {
     top_paths = find_best_paths_for_row(top_paths, i, input);
+    paths.adjust_ends(top_paths.max.index, top_paths.almost_max.index);
+  }
   // key observation: optimal path at row i is either best or second best at i -
   // 1
   return top_paths.max.val;
+}
+
+template <typename Matrix, typename PathsPolicy>
+constexpr auto solve(Matrix const& input, PathsPolicy && paths)
+{
+  auto result = input(0, 0);
+  // special case for ill-shaped matrices
+  if (input.rows() > input.cols()) {
+    result = 0;
+  }
+  else if (input.rows() <= input.cols() && input.cols() > 1)
+  {
+    result = Details::solve_non_trivial(input, paths);
+  }
+  else if (input.cols() == 1)
+    paths.init(0, 0, 0);
+  return result;
 }
 
 } // namespace Details
@@ -92,15 +160,14 @@ constexpr auto solve_non_trivial(Matrix const& input)
 template <typename Matrix, typename = MatrixTypeTraits::is_matrix_of_arithmetic_types<Matrix>>
 constexpr auto solve(Matrix const& input)
 {
-  auto result = input(0, 0);
-  // special case for ill-shaped matrices
-  if (input.rows() > input.cols())
-    result = 0;
-  else if (input.rows() <= input.cols() && input.cols() > 1)
-  {
-    result = Details::solve_non_trivial(input);
-  }
-  return result;
+  return Details::solve(input, Details::DiscardPaths{});
+}
+
+template <typename Matrix, typename = MatrixTypeTraits::is_matrix_of_arithmetic_types<Matrix>>
+auto solve_with_path(Matrix const& input) {
+  Details::StorePaths paths;
+  auto max_val = Details::solve(input, paths);
+  return std::make_pair(max_val, paths.recover_top());
 }
 
 } // namespace MaxSum
---
+++ max_matrix_sum/test/solver_path.cpp
@@ -0,0 +1,61 @@
+#include "array2d.hpp"
+#include "max_sum_solution.hpp"
+#include <catch2/catch.hpp>
+
+template <std::size_t rows, std::size_t cols>
+using Problem = Array2d<int, rows, cols>;
+
+TEST_CASE("Right paths found for problems", "[Numeric solver]")
+{
+
+  SECTION("TRIVIAL")
+  {
+    constexpr auto trivial = Problem<1u, 1u>{{1}};
+    auto expected_result = std::make_pair(1, std::vector<std::size_t>{0});
+    REQUIRE(MaxSum::solve_with_path(trivial) == expected_result);
+  }
+  SECTION("ILL FORMED")
+  {
+    constexpr auto ill_formed = Problem<5u, 3u>{};
+    auto expected_result = std::make_pair(0, std::vector<std::size_t>{});
+    REQUIRE(MaxSum::solve_with_path(ill_formed) == expected_result);
+  }
+  SECTION("NON TRIVIAL")
+  {
+    constexpr auto problem2x2_0 = Problem<2u, 2u>{{1, 0, 0, 1}};
+    auto expected_result = std::make_pair(2, std::vector<std::size_t>{0, 1});
+    REQUIRE(MaxSum::solve_with_path(problem2x2_0) == expected_result);
+
+    constexpr auto problem2x2_1 = Problem<2u, 2u>{{10, 0, 9, 0}};
+    expected_result = std::make_pair(10, std::vector<std::size_t>{0, 1});
+    auto actual = MaxSum::solve_with_path(problem2x2_1);
+    REQUIRE(MaxSum::solve_with_path(problem2x2_1) == expected_result);
+
+    constexpr auto problem2x2_2 = Problem<2u, 2u>{{10, 2, 9, 0}};
+    expected_result = std::make_pair(11, std::vector<std::size_t>{1, 0});
+    actual = MaxSum::solve_with_path(problem2x2_2);
+    REQUIRE(MaxSum::solve_with_path(problem2x2_2) == expected_result);
+
+    constexpr auto problem1x5 = Problem<1u, 5u>{{10, 2, 9, 7, 6}};
+    expected_result = std::make_pair(10, std::vector<std::size_t>{0});
+    REQUIRE(MaxSum::solve_with_path(problem1x5) == expected_result);
+
+    constexpr auto problem1x7 = Problem<1u, 7u>{{10, 2, 9, 7, 6, 12, 11}};
+    expected_result = std::make_pair(12, std::vector<std::size_t>{5});
+    REQUIRE(MaxSum::solve_with_path(problem1x7) == expected_result);
+
+    constexpr auto problem3x3 = Problem<3u, 3u>{{1, 2, 3, 5, 6, 4, 3, 2, 4}};
+    expected_result = std::make_pair(13, std::vector<std::size_t>{2, 1, 2});
+    REQUIRE(MaxSum::solve_with_path(problem3x3) == expected_result);
+
+  }
+  SECTION("MULTIPLE PATHS")
+  {
+    constexpr auto problem4x4 = Problem<4u, 4u>{{1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 4, 2, 6, 3, 5, 7}};
+    auto possible_result_1 = std::make_pair(27, std::vector<std::size_t>{2, 3, 0, 3});
+    auto possible_result_2 = std::make_pair(27, std::vector<std::size_t>{3, 2, 0, 3});
+    auto actual = MaxSum::solve_with_path(problem4x4);
+    REQUIRE((actual == possible_result_1 || actual == possible_result_2));
+  }
+}
+
