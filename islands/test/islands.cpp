a58334f3145e25effe6869aaeeca20e5a4cbb7ed
paler123 <jkowalisko@gmail.com>
2019-03-31 15:10:51 +0200

    added test cases for islands counting algorithm

--- islands/test/islands.cpp
+++ islands/test/islands.cpp
@@ -7,8 +7,24 @@ using namespace islands;
 
 TEST_CASE("correctly calculated", "[islands counting]")
 {
-  DynamicMatrix<int> single{{1, 1, 1, 1}, 2, 2};
-  REQUIRE( 1 == islands::get_number_of_islands(single));
+  DynamicMatrix<int> single{{
+    1, 1,
+    1, 1}, 2, 2};
+  REQUIRE( 1 == get_number_of_islands(single));
+
+  DynamicMatrix<int> diag{{
+    1, 0,
+    0, 1}, 2, 2};
+  REQUIRE( 4 == get_number_of_islands(diag));
+
+  DynamicMatrix<int> multiple {{
+    1, 1, 0, 1,
+    0, 1, 1, 1,
+    0, 0, 3, 3,
+    3, 3, 3, 3,
+    4, 3, 4, 3}, 5, 4};
+  REQUIRE(6 == get_number_of_islands(multiple));
 }
 
 }
+
